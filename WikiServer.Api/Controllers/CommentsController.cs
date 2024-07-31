274cdf610df2cf2394783e147fef587d8c7ae5e6
Yusuf Emrem <emrem.yusuf1@gmail.com>
2024-08-01 00:13:49 +0300

    Comment modülü oluşturuldu

--- WikiServer.Api/Controllers/CommentsController.cs
+++ WikiServer.Api/Controllers/CommentsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using WikiServer.Api.Helpers;
+using WikiServer.Api.Models;
 
 namespace WikiServer.Api.Controllers
 {
@@ -6,10 +9,60 @@ namespace WikiServer.Api.Controllers
     [ApiController]
     public class CommentsController : ControllerBase
     {
-        /*
-         * GetByFileId
-         * 
-         * 
-         */
+        [HttpGet]
+        public IActionResult ListComment()
+        {
+            var values = CommentData.List.ToArray();
+            return Ok(values);
+            //   return Ok(CommentData.List);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddComment()
+        {
+            var comment = new CommentDTO
+            {
+                AuthorName = "Deneme",
+                Content = "deneme2",
+                CreateDate = DateTime.Now,
+                FileId = 2,
+                Id = 3
+            };
+            CommentData.List.Add(comment);
+            return Ok(comment);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCommentUserById(int id)
+        {
+            var comment = CommentData.List.FirstOrDefault(x => x.Id == id);
+            if (comment == null)
+            {
+                return BadRequest();
+            }
+            return Ok(comment);
+        }
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var comment = CommentData.List.FirstOrDefault(x => x.Id == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            CommentData.List.Remove(comment);
+            return Ok();
+        }
+        [HttpGet("~/api/files/{fileId}/comments")]
+        public IActionResult GetCommentsByFileId(int fileId)
+        {
+            var comments = CommentData.List
+                .Where(c => c.FileId == fileId)
+                .ToArray();
+            return Ok(comments);
+        }
     }
 }
+/*~/ api / folders /{ folderId}/ [controller*/
